903d5265e8e134a626dd5979cc4d885cab2fc28f
Ryan Sachs <rsachs@redventures.com>
2022-09-20 16:55:09 -0400

    demo server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sachsry/demo-server
+
+go 1.18
+
+require github.com/kelseyhightower/envconfig v1.4.0
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type config struct {
+	Env     string `envconfig:"ENV" required:"true"`
+	DevURL  string `envconfig:"DEV_URL"`
+	ProdURL string `envconfig:"PROD_URL"`
+}
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/demo", DemoConfig)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, from %s!", r.Host)
+}
+
+func DemoConfig(w http.ResponseWriter, r *http.Request) {
+	var c config
+	err := envconfig.Process("", &c)
+	resp := make(map[string]string)
+	if err != nil {
+		resp["error"] = err.Error()
+	} else {
+		resp["env"] = c.Env
+		resp["devUrl"] = c.DevURL
+		resp["prodUrl"] = c.ProdURL
+	}
+
+	json, marshalErr := json.Marshal(resp)
+	if marshalErr != nil {
+		fmt.Fprintf(w, "encountered unexpected error: %s", marshalErr.Error())
+		return
+	}
+
+	w.Write(json)
+}
